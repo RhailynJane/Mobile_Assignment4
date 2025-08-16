0f083e61123ca72b7ccdd96dc346396abbe0fa7a
Komalpreet Kaur <komalpreetkaur05@edu.sait.ca>
2025-08-16 12:46:54 -0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC-3fi7HMpz2rp9GbIcRLzrP8FzVyYHapc",
+  authDomain: "assignment4-4b951.firebaseapp.com",
+  projectId: "assignment4-4b951",
+  storageBucket: "assignment4-4b951.firebasestorage.app",
+  messagingSenderId: "287394544811",
+  appId: "1:287394544811:web:ec4cae98262562bd82cbee"
 };
 
 const app = initializeApp(firebaseConfig);
